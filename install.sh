fce77e25c512d57ccd07df74a8bb999992f3ca3e
sohaibnoman <42720743+sohaibnoman@users.noreply.github.com>
2019-04-29 19:56:54 +0200

    make install.h fix command-t

--- install.sh
+++ install.sh
@@ -11,6 +11,13 @@ for filname in $(pwd)/.*; do
     fi
 done 
 
+# compile vim and bash
 source ~/.bashrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall +qall
-cd .. && rm -rf dotfiles && cd ~/
+
+# remove folder
+cd .. && rm -rf dotfiles
+
+# make command-t work for vim
+sudo dnf install ruby-devel
+cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t && ruby extconf.rb && make clean && make

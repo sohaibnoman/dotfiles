05eddc0e599379b2ace850fad2d96b562d510ff5
sohaibnoman <42720743+sohaibnoman@users.noreply.github.com>
2019-04-29 15:23:11 +0200

    Make location of dotfiles more dynamic

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for filname in ~/dotfiles/.*; do  
+for filname in $(pwd)/.*; do  
     if [ $(basename $filname) != ".git" ] && [ $(basename $filname) != "." ] && [ $(basename $filname) != ".." ]
     then 
         mv $(basename $filname) ~/$(basename $filename);

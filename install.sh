172c36d832c4f400c116cfc1413b29b80e54220f
Sohaib Ahmed <sohaib.n@icloud.com>
2019-05-14 20:04:55 +0200

    made symlinks instead

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ for filname in $(pwd)/.*; do
             then
                 rm -rf ~/$(basename $filname);
             fi
-        mv $(basename $filname) ~/;
+            ln -s $(pwd)/$(basename $filname) ~/;
     fi
 done 
 
@@ -16,8 +16,8 @@ source ~/.bashrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall +qall
 
 # remove folder
-cd .. && rm -rf dotfiles
+# cd .. && rm -rf dotfiles
 
 # make command-t work for vim
-sudo dnf install ruby-devel
-cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t && ruby extconf.rb && make clean && make
+# sudo dnf install ruby-devel
+# cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t && ruby extconf.rb && make clean && make

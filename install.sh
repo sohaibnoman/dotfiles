61255194ec1a684b90f6516608ddd32258c7958d
Sohaib Ahmed <sohaib.n@icloud.com>
2019-05-30 23:19:40 +0200

    Remove i3 info

--- install.sh
+++ install.sh
@@ -14,15 +14,17 @@ do
         read -p "Do you want paper-icons [1] or flat-remix-icons [2]: " icons_coise
         if [ $icons_coise == "1" ]
         then 
-            sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:snwh:paper/Fedora_25/home:snwh:paper.repo
-            sudo dnf install paper-icon-theme
+            sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:snwh:paper/Fedora_25/home:snwh:paper.repo && sudo dnf install paper-icon-theme
         elif [ $icons_coise == "2" ]
         then 
 		    sudo dnf copr enable daniruiz/flat-remix && sudo dnf install flat-remix
         else
             echo "Your choise dosent exist no icon pack installed"
         fi
-        
+
+        # make command-t work for vim
+        # sudo dnf install ruby-devel
+        # cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t && ruby extconf.rb && make clean && make
         break;
     elif [ $user_dep == "N" ] || [ $user_dep == "n" ]
     then 
@@ -51,13 +53,5 @@ done
 
 # compile vim and bash
 source ~/.bashrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
-vim +PluginInstall +qall
-
-# remove folder
-# cd .. && rm -rf dotfiles
-
-# make command-t work for vim
-# sudo dnf install ruby-devel
-# cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t && ruby extconf.rb && make clean && make
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall +qall
 
